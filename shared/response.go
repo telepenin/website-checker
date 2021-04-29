2df4b3f9eb49f10d2389f2f97178f6bb26d2b682
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2021-04-29 16:27:08 +0300

    Using ToJson method for the response

--- shared/response.go
+++ shared/response.go
@@ -1,8 +1,14 @@
 package shared
 
+import "encoding/json"
+
 type Response struct {
 	Website  Website
 	Code     int      `json:"code"`
 	Duration float64  `json:"duration"`
 	Content  [][]byte `json:"content"`
+}
+
+func (r *Response) ToJson() ([]byte, error) {
+	return json.Marshal(r)
 }
